67dd67f644b488c4c7e1c741b55cfd18a4a723fb
toni-od <samaelsamael662@gmail.com>
2025-01-03 20:43:01 +0300

    feat: add a simplistic router and fs
    a file system to separate concerns
    a router to handle requests

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<h1>Awesome</h1>
 <?php
-include_once __DIR__ . "/second.php";
-echo "chance!" . "<br />";
-echo "successfully set up";
+
+$path = $_SERVER["REQUEST_URI"];
+echo $path . "<br>";
+
+if($path == "/core") {
+    include_once __DIR__ . "/web/index.php";
+} else if($path == "/") {
+    include_once __DIR__ . "/core/index.php";
+} else {
+    echo "<h1>404, Page not found!</h1>";
+}
